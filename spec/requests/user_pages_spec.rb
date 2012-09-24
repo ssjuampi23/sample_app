54b99bd7206c9c4b8c7ab1ab3629ea394d8b20ba
ssjuampi23 <sspablo23@hotmail.com>
2012-09-24 07:04:06 -0600

    end exercise 1 of 2 chapter 8

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -75,4 +75,22 @@ describe "UserPages" do
   end # end sign up describe block
   
   
+  describe "edit" do
+    let(:user){ FactoryGirl.create(:user) }#code to make a user variable
+    #before { visit edit_user_path(user)}
+  
+    describe "page" do
+     # it {should have_selector('h1', text: "Update your profile")}
+     # it {should have_selector('title', text: "Edit User")}
+     # it {should have_link('change', href: 'http://gravatar.com/emails')}
+    end
+  
+    describe "with invalid information" do
+     # before { click_button "Save changes" }
+    
+     # it{ should have_content('error') }
+    end
+  
+  end # end edit describe block
+  
 end
