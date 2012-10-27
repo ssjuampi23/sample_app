462e9ce2abea22aceda24f2932e1cc47ae4ab553
ssjuampi23 <sspablo23@hotmail.com>
2012-10-27 12:40:27 -0600

    10.3.2 in progress

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,6 +3,13 @@ class MicropostsController < ApplicationController
  before_filter :signed_in_user, only: [:create, :destroy]
  
  def create
+  @micropost = current_user.microposts.build(params[:micropost]) # the @micropost variable is being created here, then it can be reached in the view
+  if @micropost.save
+    flash[:success] = "Micropost created!"
+    redirect_to root_url
+  else
+    render 'static_pages/home'
+  end
  end
  
  def destroy
