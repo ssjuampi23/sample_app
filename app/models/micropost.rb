93cdf4a3cab2048d57ef59e5d5fcbfbbed47826a
ssjuampi23 <sspablo23@hotmail.com>
2012-12-13 06:04:07 -0600

    11.3.2 finished

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -8,4 +8,10 @@ class Micropost < ActiveRecord::Base
   
   default_scope order: 'microposts.created_at DESC' # DESC means descending (from newest to older) which is a SQL syntax
                                                     #default scope also gets the "should have the right microposts in the right order" test to pass
+                                                    
+  def self.from_users_followed_by(user)
+    followed_user_ids = user.followed_user_ids #user.followed_user_ids will return an array containing all the IDS of the users the user (parameter) is following
+    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+  end                                                    
+                                                    
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -203,12 +203,25 @@ describe User do
         FactoryGirl.create(:micropost,user:FactoryGirl.create(:user))
       end
       
+      let(:followed_user){ FactoryGirl.create(:user)}
+      
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") } # 3 different microposts are created for the followed user
+      end
+      
       its(:feed){ should include(newer_micropost) }
       its(:feed){ should include(older_micropost)}
       its(:feed){ should_not include(unfollowed_post)}
+      its(:feed) do
+        followed_user.microposts.each do |micropost|
+          should include(micropost)
+          end
+      end
     
     end # end status
-  
+    
+    
   end #end microposts associations
   
   describe "following" do
