57ac10ee0bfcd6052bf2c5789b6bcac94428163b
ssjuampi23 <sspablo23@hotmail.com>
2012-12-13 22:41:12 -0600

    11.3 finished

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -10,8 +10,9 @@ class Micropost < ActiveRecord::Base
                                                     #default scope also gets the "should have the right microposts in the right order" test to pass
                                                     
   def self.from_users_followed_by(user)
-    followed_user_ids = user.followed_user_ids #user.followed_user_ids will return an array containing all the IDS of the users the user (parameter) is following
-    where("user_id IN (?) OR user_id = ?", followed_user_ids, user)
+    followed_user_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id", user_id: user.id) # Returns microposts from the users being followed by the given user.
+    #where("user_id IN ?) OR user_id = ?", followed_user_ids, user)
   end                                                    
                                                     
 end
